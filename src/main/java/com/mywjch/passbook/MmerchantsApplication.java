a1e6ea6a7e5bd622b6c02aa16bf783f56ad3eb9c
wjch <jiechao.wang@aiaudit.cn>
2019-10-11 17:53:23 +0800

    Initial commit

---
+++ src/main/java/com/mywjch/passbook/MmerchantsApplication.java
@@ -0,0 +1,13 @@
+package com.mywjch.passbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmerchantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmerchantsApplication.class, args);
+    }
+
+}
